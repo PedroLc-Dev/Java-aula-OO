5d91cb108cd4937351ebed861e290de813cb4b41
Pedro Lucas <pedrolc.nogueira@outlook.com>
2025-04-03 00:57:45 -0300

    primeiro dia

--- src/com/aula/cefet/Main.java
+++ src/com/aula/cefet/Main.java
@@ -1,10 +1,89 @@
 package com.aula.cefet;
 
 public class Main {
+	
+	/*
+	 *  Criar três variaveis de número inteiro
+	 *  três variaveis de texto
+	 *  
+	 *  printar na tela o texto e o numero no formato  texto: numero
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Exercicio 1
+		
+		String varTexto1;
+		String varTexto2;
+		String varTexto3;
+		
+		int varNum1;
+		int varNum2;
+		int varNum3;
+		
+		varTexto1 ="Pedro";
+		varTexto2 ="Anthoni";
+		varTexto3 ="Stela";
+		
+		varNum1 =2;
+		varNum2 =4;
+		varNum3 =5;
+		
+		System.out.println(varTexto1 + ": " + varNum1);
+		System.out.println(varTexto2 + ": " + varNum2);
+		System.out.println(varTexto3 + ": " + varNum3);
+		
+		//Exercicio 2
+		
+		String nomeChar1;
+		String nomeChar2;
+		String nomeChar3;
+		
+		int strikeChar1;
+		int strikeChar2;
+		int strikeChar3;
+		
+		String specialMoveChar1;
+		String specialMoveChar2;
+		String specialMoveChar3;
+		
+		
+		nomeChar1 = "Gogeta";
+		nomeChar2 = "Vegetto";
+		nomeChar3 = "Gohan";
+		
+		strikeChar1 = 10;
+		strikeChar2 = 20;
+		strikeChar3 = 30;
+		
+		specialMoveChar1 = "aoe";
+		specialMoveChar2 = "counter";
+		specialMoveChar3 = "buff";
+		
+		System.out.println("--------------------------------------------------");
+		System.out.printf(nomeChar3 + "\n" +strikeChar3 +"\n" + specialMoveChar3 + "\n");
+		System.out.printf(nomeChar2 + "\n" +strikeChar2 +"\n" + specialMoveChar2 + "\n");
+		System.out.printf(nomeChar1 + "\n" +strikeChar1 +"\n" + specialMoveChar1 + "\n");
+		System.out.println("--------------------------------------------------");
+		
+		strikeChar3 = 50;
+		
+		System.out.printf(nomeChar3 + "\n" +strikeChar3 +"\n" + specialMoveChar3 + "\n");
+		
+		System.out.println("-------------Exercicio 3----------------------");
+		
+		
+		Personagem gohan = new Personagem();
+		gohan.nome = "gohan";
+		gohan.specialMove = "buff";
+		gohan.strike = 10;
+		
+		System.out.printf(gohan.nome + "\n" + gohan.strike +"\n" + gohan.specialMove + "\n");
+		
+		System.out.println("-------------printando com metodo-------------------");
+		
+		gohan.printar();
+		
 	}
 
 }
---
+++ src/com/aula/cefet/Personagem.java
@@ -0,0 +1,16 @@
+package com.aula.cefet;
+
+public class Personagem {
+	String nome;
+	int strike;
+	String specialMove;
+	
+	
+	public void printar() {
+		System.out.printf(nome + "\n" + strike +"\n" + specialMove + "\n");
+		
+		
+	}
+	
+	
+}
